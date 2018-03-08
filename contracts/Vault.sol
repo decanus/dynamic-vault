8446e5e3012b9c241ec440ee0f3d0212af42cda3
Dean Eigenmann <dean@tokenate.io>
2018-03-08 04:01:26 +0100

    Rearranged

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -6,14 +6,14 @@ import "./Connectors/TokenConnectorInterface.sol";
 
 contract Vault is Ownable, VaultInterface {
 
-    mapping (address => TokenConnectorInterface) connectors;
-
-    function addConnector(address token, TokenConnectorInterface connector) public onlyOwner {
-        connectors[token] = connector;
-    }
+    mapping (address => TokenConnectorInterface) public connectors;
 
     function withdraw(address token, uint amount) external {
         connectors[token].withdraw(token, msg.sender, amount);
+    }
+    
+    function addConnector(address token, TokenConnectorInterface connector) public onlyOwner {
+        connectors[token] = connector;
     }
 
     function balanceOf(address token) public view returns (uint) {
